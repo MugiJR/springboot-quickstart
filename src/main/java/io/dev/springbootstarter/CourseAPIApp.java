362cd27efce7794cda9be71e121e2327f70f084d
Mugesh <38450659+MugiJR@users.noreply.github.com>
2022-08-03 22:49:02 +0200

    Created Merge conflict
    Ignore this. Just for my practice

--- src/main/java/io/dev/springbootstarter/CourseAPIApp.java
+++ src/main/java/io/dev/springbootstarter/CourseAPIApp.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CourseAPIApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(CourseAPIApp.class, args);
+        SpringApplicationWRONG.run(CourseAPIApp.class, args);
     }
 }
