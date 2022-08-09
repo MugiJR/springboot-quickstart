0bd521b819d23faacb84942a98173c9966ab102a
MugiJR <immugi@yahoo.com>
2022-08-09 12:38:05 +0200

    Step 1 : JPA -> Entity Mapping for TOPIC

--- src/main/java/io/dev/springbootstarter/topics/Topic.java
+++ src/main/java/io/dev/springbootstarter/topics/Topic.java
@@ -1,7 +1,12 @@
 package io.dev.springbootstarter.topics;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Topic {
 
+    @Id
     public String id;
     public String name;
     public String description;
